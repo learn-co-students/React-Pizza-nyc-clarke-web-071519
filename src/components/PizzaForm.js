cf3ec3089d304c12eadeae255cb600377c2428f2
Valentino Wong <valentino.wong@gmail.com>
2019-09-23 00:09:58 -0400

    Done.

--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,41 +1,56 @@
 import React from "react"
 
-const PizzaForm = () => {
+const PizzaForm = (props) => {
+  const {topping, size, vegetarian} = props.pizza
+
+  const handleSubmit = () => {
+    props.editPizza();
+  }
+
   return(
-      <div className="form-row">
-        <div className="col-5">
-            <input type="text" className="form-control" placeholder="Pizza Topping" value={
-                //Pizza Topping Should Go Here
-                null
-              }/>
+    <div className="form-row">
+      <div className="col-5">
+          <input 
+            name="topping"
+            type="text" 
+            className="form-control" 
+            placeholder="Pizza Topping" 
+            value={topping}
+            onChange={props.editPizzaTopping}
+            />
+      </div>
+      <div className="col">
+        <select 
+          name="size" 
+          value={size} 
+          className="form-control"
+          onChange={props.editPizzaSize}
+          >
+          <option value="Small">Small</option>
+          <option value="Medium">Medium</option>
+          <option value="Large">Large</option>
+        </select>
+      </div>
+      <div className="col">
+        <div className="form-check">
+          <input name="vegetarian" className="form-check-input" type="radio" value="Vegetarian" checked={vegetarian} onChange={props.markPizzaVegetarian}/>
+          <label className="form-check-label">
+            Vegetarian
+          </label>
         </div>
-        <div className="col">
-          <select value={null} className="form-control">
-            <option value="Small">Small</option>
-            <option value="Medium">Medium</option>
-            <option value="Large">Large</option>
-          </select>
-        </div>
-        <div className="col">
-          <div className="form-check">
-            <input className="form-check-input" type="radio" value="Vegetarian" checked={null}/>
-            <label className="form-check-label">
-              Vegetarian
-            </label>
-          </div>
-          <div className="form-check">
-            <input className="form-check-input" type="radio" value="Not Vegetarian" checked={null}/>
-            <label className="form-check-label">
-              Not Vegetarian
-            </label>
-          </div>
-        </div>
-        <div className="col">
-          <button type="submit" className="btn btn-success" onClick={console.log}>Submit</button>
+        <div className="form-check">
+          <input name="notVegetarian" className="form-check-input" type="radio" value="Not Vegetarian" checked={!vegetarian} onChange={props.markPizzaNotVegetarian}/>
+          <label className="form-check-label">
+            Not Vegetarian
+          </label>
         </div>
       </div>
-
+      <div className="col">
+        <button type="submit" className="btn btn-success" onClick={handleSubmit}>Submit</button>
+      </div>
+    </div>
   )
+  
 }
 
 export default PizzaForm
